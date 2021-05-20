bff6fa3b7f0860f4f677237c68cb16d6f57397ca
edjunma <edjunma@gmail.com>
2021-05-20 00:02:40 -0400

    working on feature section

--- src/sections/key-feature.js
+++ src/sections/key-feature.js
@@ -54,7 +54,13 @@ export default function KeyFeature() {
 
         <Grid sx={styles.grid}>
           {data.map((item) => (
-            <FeatureCard />
+            <FeatureCardColumn
+              key={item.id}
+              src={item.imgSrc}
+              alt={item.altText}
+              title={item.title}
+              text={item.text}
+              />
           ))}
         </Grid>
      </Container>
